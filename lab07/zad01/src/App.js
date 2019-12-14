4a6d13fb00fe98736efc7635d4b4ccf5ae44750c
Przemyslaw Wiszowaty <przemyslaw.wiszowaty@softwarehut.com>
2019-12-14 15:14:57 +0100

    lab07

---
+++ lab07/zad01/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import AboutPage from './components/AboutPage/AboutPage';
+import ContactPage from './components/ContactPage/ContactPage';
+import HomePage from './components/HomePage/HomePage';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+      <AboutPage />
+      <ContactPage />
+    </div>
+  );
+}
+
+export default App;
